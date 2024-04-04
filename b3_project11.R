ab4ca9137df07d954b69d232f5a34a99b560a629
Melanie De Nolf <melaniedenolf@outlook.com>
2024-04-04 11:36:43 +0200

    Font and bug fix
    Title font changed to B-cubed default, bug with download button fixed

--- b3_project11.R
+++ b3_project11.R
@@ -22,7 +22,9 @@ ui <- fluidPage(
     tags$title("B³ Indicators"),
     tags$link(rel="icon", type="image/png", size="32x32", href="B3_logomark.png"),
     tags$meta(name="viewport", content="width=device-width"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$link(href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&display=swap", 
+              rel="stylesheet")
   ),
 
   # input = text fields, action buttons
